126679d5882db92743bb918c905174e83d55ef86
Mauro Perez <mperez@kssoluciones.com>
2024-07-20 21:31:06 -0600

    feat(1.0.0.0) Primer Commit AES

---
+++ AesSecutiry/src/main/java/com/tools/ks/AesSecutiryApplication.java
@@ -0,0 +1,13 @@
+package com.tools.ks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AesSecutiryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AesSecutiryApplication.class, args);
+	}
+
+}
